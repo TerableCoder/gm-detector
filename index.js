0350217e90c065c3bcb9bc27e73ddecbe18af333
Tera Coder <31596711+TerableCoder@users.noreply.github.com>
2019-01-03 11:39:31 -0500

    changed chats
    I forget what channel 7(probably outdated), using 21(notice now)
    S_DUNGEON_EVENT_MESSAGE is the same thing as skill-resets, rather use proxy chat+notice though

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 module.exports = function GmDetector(dispatch) {
-
+	const command = dispatch.command || dispatch.require.command;
 	dispatch.hook('S_SPAWN_USER', 13, event => {
         if (event.gm) {
             let message = '[GM] ' + event.name + ' Detected!';
             if (event.gmInvisible) message += ' (Invisible)';           
             
-            // send messages to client
-            dispatch.toClient('S_CHAT', 1, {
-                channel: 7,
-                authorName: '',
-                message: message
-            }); 
-            dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 2, {
-                type: 2,
-                message: message,
-				chat: false,
-				channel: 0
-            }); 
+            // send messages to notice and proxy chat
+			dispatch.toClient('S_CHAT', 2, { channel: 21, authorName: 'GM-Detector', message: message});
+            command.message(msg);
             
             // show hidden GMs?
             event.gmInvisible = false;
             return true;
         }
     });
-    
 }
