7a41d935d7fb335cdab9533bb76382b049846eac
Tera Coder <TerableCoder@gmail.com>
2019-09-28 20:48:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function GmDetector(dispatch) {
 	const command = dispatch.command || dispatch.require.command;
-	dispatch.hook('S_SPAWN_USER', 13, event => {
+	dispatch.hook('S_SPAWN_USER', 15, event => {
         if (event.gm) {
             let message = '[GM] ' + event.name + ' Detected!';
             if (event.gmInvisible) message += ' (Invisible)';           
             
             // send messages to notice and proxy chat
-			dispatch.toClient('S_CHAT', 2, { channel: 21, authorName: 'GM-Detector', message: message});
+			dispatch.toClient('S_CHAT', 3, { channel: 21, name: 'GM-Detector', message: message});
             command.message(msg);
             
             // show hidden GMs?
